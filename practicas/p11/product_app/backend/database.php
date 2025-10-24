f5744327598458bd67432f6047f84f11989774b8
Daniela Lira <146269037+danielalira20@users.noreply.github.com>
2025-10-24 14:26:46 -0600

    Inicio de practica 11

---
+++ practicas/p11/product_app/backend/database.php
@@ -0,0 +1,15 @@
+<?php
+    $conexion = @mysqli_connect(
+        'localhost',
+        'root',
+        'daniela20',
+        'marketzone'
+    );
+
+    /**
+     * NOTA: si la conexión falló $conexion contendrá false
+     **/
+    if(!$conexion) {
+        die('¡Base de datos NO conextada!');
+    }
+?>
