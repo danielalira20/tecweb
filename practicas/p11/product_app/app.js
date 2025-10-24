68d72415017e9aeedb16b640dc694e31d3e865bd
Daniela Lira <146269037+danielalira20@users.noreply.github.com>
2025-10-24 14:32:25 -0600

    Implementar listado y búsqueda con jQuery

--- practicas/p11/product_app/app.js
+++ practicas/p11/product_app/app.js
@@ -8,11 +8,108 @@ var baseJSON = {
     "imagen": "img/default.png"
 };
 
+// Se ejecuta cuando el documento está listo
+$(document).ready(function() {
+    init();
+    listarProductos();
+    
+    // Búsqueda en tiempo real
+    $('#search').keyup(function() {
+        let search = $(this).val();
+        if(search.length > 0) {
+            buscarProducto(search);
+        } else {
+            listarProductos();
+            $('#product-result').addClass('d-none');
+        }
+    });
+});
+
 function init() {
-    /**
-     * Convierte el JSON a string para poder mostrarlo
-     * ver: https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/JSON
-     */
     var JsonString = JSON.stringify(baseJSON, null, 2);
-    document.getElementById("description").value = JsonString;
+    $('#description').val(JsonString);
+}
+
+// Listar todos los productos
+function listarProductos() {
+    $.ajax({
+        url: './backend/product-list.php',
+        type: 'GET',
+        success: function(response) {
+            let productos = JSON.parse(response);
+            
+            if(Object.keys(productos).length > 0) {
+                let template = '';
+                
+                productos.forEach(producto => {
+                    let descripcion = '';
+                    descripcion += '<li>precio: ' + producto.precio + '</li>';
+                    descripcion += '<li>unidades: ' + producto.unidades + '</li>';
+                    descripcion += '<li>modelo: ' + producto.modelo + '</li>';
+                    descripcion += '<li>marca: ' + producto.marca + '</li>';
+                    descripcion += '<li>detalles: ' + producto.detalles + '</li>';
+                    
+                    template += `
+                        <tr productId="${producto.id}">
+                            <td>${producto.id}</td>
+                            <td>${producto.nombre}</td>
+                            <td><ul>${descripcion}</ul></td>
+                            <td>
+                                <button class="product-delete btn btn-danger">
+                                    Eliminar
+                                </button>
+                            </td>
+                        </tr>
+                    `;
+                });
+                
+                $('#products').html(template);
+            }
+        }
+    });
+}
+
+// Buscar productos
+function buscarProducto(search) {
+    $.ajax({
+        url: './backend/product-search.php',
+        type: 'GET',
+        data: { search: search },
+        success: function(response) {
+            let productos = JSON.parse(response);
+            
+            if(Object.keys(productos).length > 0) {
+                let template = '';
+                let template_bar = '';
+                
+                productos.forEach(producto => {
+                    let descripcion = '';
+                    descripcion += '<li>precio: ' + producto.precio + '</li>';
+                    descripcion += '<li>unidades: ' + producto.unidades + '</li>';
+                    descripcion += '<li>modelo: ' + producto.modelo + '</li>';
+                    descripcion += '<li>marca: ' + producto.marca + '</li>';
+                    descripcion += '<li>detalles: ' + producto.detalles + '</li>';
+                    
+                    template += `
+                        <tr productId="${producto.id}">
+                            <td>${producto.id}</td>
+                            <td>${producto.nombre}</td>
+                            <td><ul>${descripcion}</ul></td>
+                            <td>
+                                <button class="product-delete btn btn-danger">
+                                    Eliminar
+                                </button>
+                            </td>
+                        </tr>
+                    `;
+                    
+                    template_bar += `<li>${producto.nombre}</li>`;
+                });
+                
+                $('#product-result').removeClass('d-none').addClass('d-block');
+                $('#container').html(template_bar);
+                $('#products').html(template);
+            }
+        }
+    });
 }
