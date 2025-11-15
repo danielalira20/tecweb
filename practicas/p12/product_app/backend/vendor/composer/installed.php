58d19e04afc6b1ad5cf3a0f1d9a5230166a63d03
Daniela Lira <146269037+danielalira20@users.noreply.github.com>
2025-11-14 18:26:07 -0600

    refactor: reorganize classes into CRUD folders

---
+++ practicas/p12/product_app/backend/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'daniela/product-app',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6b9602d3223d8768ad7a3df65a5c0c2283135092',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'daniela/product-app' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6b9602d3223d8768ad7a3df65a5c0c2283135092',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
