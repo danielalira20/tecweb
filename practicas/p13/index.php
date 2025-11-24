fbacca7cbc2407a5cb9b3b5f226f0fe47072df64
Daniela Lira <146269037+danielalira20@users.noreply.github.com>
2025-11-23 21:29:12 -0600

    Inicialización de práctica REST Slim v4 y dependencias Composer

---
+++ practicas/p13/index.php
@@ -0,0 +1,18 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require 'vendor/autoload.php';
+
+$app = AppFactory::create();
+$app->setBasePath('/tecweb/practicas/p13'); 
+
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write("¡Hola mundo Slim 4!");
+    return $response;
+});
+
+$app->run();
+
+?>
